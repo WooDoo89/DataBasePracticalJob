55ed5c14b573a2f91ab90a450a21869b4107dbb8
Vitalij Gurin <vitalg16@gmail.com>
2020-05-03 00:01:32 +0300

    Started connection of a database to a program

---
+++ DataBasePracticalJob/DataBase.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBasePracticalJob
+{
+    class DataBase
+    {
+        public static List<PaymentClass> GetPayment(string TableName)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<PaymentClass>("select * from " + TableName, new DynamicParameters());
+                return output.ToList();
+            }
+        }
+        public static void SaveBoughtGames(string TableName, BoughtGames Bg)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into BoughtGames values (@ID, @Name)", Bg);
+            }
+        }
+        private static string LoadConnectionString(string id = "praktikosDB")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
